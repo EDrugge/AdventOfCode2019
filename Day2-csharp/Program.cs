3febf934a95824cc86b799ade2394dae3563fe50
Emil Drugge <emil@drugge.com>
2019-12-06 09:57:30 +0100

    Added solution

---
+++ Day2-csharp/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Linq;
+using Console = System.Console;
+
+namespace Day2_csharp
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var originalCode = new[] { 1, 12, 2, 3, 1, 1, 2, 3, 1, 3, 4, 3, 1, 5, 0, 3, 2, 1, 10, 19, 1, 9, 19, 23, 1, 13, 23, 27, 1, 5, 27, 31, 2, 31, 6, 35, 1, 35, 5, 39, 1, 9, 39, 43, 1, 43, 5, 47, 1, 47, 5, 51, 2, 10, 51, 55, 1, 5, 55, 59, 1, 59, 5, 63, 2, 63, 9, 67, 1, 67, 5, 71, 2, 9, 71, 75, 1, 75, 5, 79, 1, 10, 79, 83, 1, 83, 10, 87, 1, 10, 87, 91, 1, 6, 91, 95, 2, 95, 6, 99, 2, 99, 9, 103, 1, 103, 6, 107, 1, 13, 107, 111, 1, 13, 111, 115, 2, 115, 9, 119, 1, 119, 6, 123, 2, 9, 123, 127, 1, 127, 5, 131, 1, 131, 5, 135, 1, 135, 5, 139, 2, 10, 139, 143, 2, 143, 10, 147, 1, 147, 5, 151, 1, 151, 2, 155, 1, 155, 13, 0, 99, 2, 14, 0, 0 };
+			var programCode = new[] { 1, 12, 2, 3, 1, 1, 2, 3, 1, 3, 4, 3, 1, 5, 0, 3, 2, 1, 10, 19, 1, 9, 19, 23, 1, 13, 23, 27, 1, 5, 27, 31, 2, 31, 6, 35, 1, 35, 5, 39, 1, 9, 39, 43, 1, 43, 5, 47, 1, 47, 5, 51, 2, 10, 51, 55, 1, 5, 55, 59, 1, 59, 5, 63, 2, 63, 9, 67, 1, 67, 5, 71, 2, 9, 71, 75, 1, 75, 5, 79, 1, 10, 79, 83, 1, 83, 10, 87, 1, 10, 87, 91, 1, 6, 91, 95, 2, 95, 6, 99, 2, 99, 9, 103, 1, 103, 6, 107, 1, 13, 107, 111, 1, 13, 111, 115, 2, 115, 9, 119, 1, 119, 6, 123, 2, 9, 123, 127, 1, 127, 5, 131, 1, 131, 5, 135, 1, 135, 5, 139, 2, 10, 139, 143, 2, 143, 10, 147, 1, 147, 5, 151, 1, 151, 2, 155, 1, 155, 13, 0, 99, 2, 14, 0, 0 };
+
+			var currentIndex = 0;
+
+			while (true)
+			{
+				var command = programCode[currentIndex];
+
+				switch (command)
+				{
+					case 1:
+						var first = programCode[programCode[currentIndex + 1]];
+						var second = programCode[programCode[currentIndex + 2]];
+						var sum = first + second;
+						programCode[programCode[currentIndex + 3]] = sum;
+						if (programCode[0] == 19690720)
+						{
+							Console.WriteLine("Hej");
+							return;
+						}
+						//originalCode.ToList().CopyTo(programCode);
+						currentIndex += 4;
+						break;
+					case 2:
+						var first2 = programCode[programCode[currentIndex + 1]];
+						var second2 = programCode[programCode[currentIndex + 2]];
+						var product = first2 * second2;
+						programCode[programCode[currentIndex + 3]] = product;
+						if (programCode[0] == 19690720)
+						{
+							Console.WriteLine("Hej");
+							return;
+						}
+						//originalCode.ToList().CopyTo(programCode);
+						currentIndex += 4;
+						break;
+					case 99:
+						Console.WriteLine(programCode.Aggregate("", (s, i) => s + i + ", "));
+						return;
+				}
+			}
+
+		}
+	}
+}
---
+++ Day2/Program.fs
@@ -0,0 +1,32 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+
+let executeCommand position programCode =
+    //let command = programCode[position]
+    //if (command = 1) then
+    //    let mutable index = 0
+    //    let value1 = programCode[position+1]
+    //    let value2 = programCode[position+2]
+    //    let sum = value1 + value2
+        
+    //let mutable index = 0
+
+    //let test = 
+    //    programCode
+    //    |> List.map (fun (v) -> index <- index + 1, v)
+
+
+    //Console.WriteLine test
+
+    0
+
+[<EntryPoint>]
+let main argv =
+    let programCode = [1;9;10;3;2;3;11;0;99;30;40;50]
+
+    let currentPosition = 0
+
+
+
+    0 // return an integer exit code
