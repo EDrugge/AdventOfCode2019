c8f2ce39bbd830f707cb5c51195ce90659a3364e
Emil Drugge <emil@drugge.com>
2019-12-23 21:54:12 +0100

    Added solution to day 4

---
+++ Day4-csharp/PasswordValidator.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using FluentAssertions;
+using Xunit;
+
+namespace Day4_csharp
+{
+    public class PasswordValidator
+    {
+        public bool IsValid(string password)
+        {
+            var isValid = false;
+
+            char? previousChar = null;
+            int currentStreakOfAdjecentDigits = 1;
+            bool hasAGroupOfTwoAdjecentDigits = false;
+            foreach (var digit in password)
+            {
+                if (previousChar != null)
+                {
+                    if (previousChar == digit)
+                    {
+                        currentStreakOfAdjecentDigits++;
+                    }
+                    else
+                    {
+                        if (currentStreakOfAdjecentDigits == 2)
+                        {
+                            hasAGroupOfTwoAdjecentDigits = true;
+                        }
+
+                        currentStreakOfAdjecentDigits = 1;
+                    }
+
+                    if (Convert.ToInt32(previousChar.Value) > Convert.ToInt32(digit))
+                    {
+                        return false;
+                    }
+                }
+                previousChar = digit;
+            }
+
+            if (currentStreakOfAdjecentDigits == 2)
+            {
+                hasAGroupOfTwoAdjecentDigits = true;
+            }
+
+            return hasAGroupOfTwoAdjecentDigits;
+        }
+    }
+
+    public class PasswordValidatorTests
+    {
+        [Theory]
+        [InlineData("123456", false)]
+        [InlineData("122456", true)]
+        public void MustHaveAtLeastTwoAdjacentDigits(string password, bool isValid)
+        {
+            var sut = new PasswordValidator();
+
+            sut.IsValid(password).Should().Be(isValid);
+        }
+
+        [Theory]
+        [InlineData("123444", false)]
+        [InlineData("111122", true)]
+        [InlineData("112233", true)]
+        public void MustHaveAtLeastOneGroupWithOnlyTwoAdjacentDigits(string password, bool isValid)
+        {
+            var sut = new PasswordValidator();
+
+            sut.IsValid(password).Should().Be(isValid);
+        }
+
+        [Theory]
+        [InlineData("223450", false)]
+        [InlineData("112234", true)]
+        public void DigitsShouldNeverDecrease(string password, bool isValid)
+        {
+            var sut = new PasswordValidator();
+
+            sut.IsValid(password).Should().Be(isValid);
+        }
+    }
+}
---
+++ Day4-csharp/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Day4_csharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numberOfValidPasswords = 0;
+
+            var passwordValidator = new PasswordValidator();
+
+            foreach (var i in Sequence(264793, 803935))
+            {
+                if (passwordValidator.IsValid(i.ToString()))
+                {
+                    numberOfValidPasswords++;
+                }
+            }
+
+            Console.WriteLine($"Number of valid passwords: {numberOfValidPasswords}");
+        }
+
+        private static IEnumerable<int> Sequence(int n1, int n2)
+        {
+            while (n1 <= n2)
+            {
+                yield return n1++;
+            }
+        }
+    }
+}
